6eb2ce6633d0412b50be2fd86ee879f6d305671b
Mac <>
2018-07-16 10:01:35 +0800

    初始话

---
+++ Song/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Song
+//
+//  Created by Mac on 2018/7/16.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+	//宋照界
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
